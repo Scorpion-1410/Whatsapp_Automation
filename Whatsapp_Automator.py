0b812b88b3b36598e627e6f3b2d7b74f25680c31
Tanishq14 <46865902+Tanishq14@users.noreply.github.com>
2021-01-20 21:08:37 +0530

    Modified Version

---
+++ Whatsapp_Automator.py
@@ -0,0 +1,249 @@
+from tkinter import *
+from tkinter.ttk import *
+import pywhatkit
+import pywhatkit.mainfunctions
+from PIL import Image, ImageTk
+import os
+
+# Creating the GUI Window
+root = Tk() 
+root.geometry('640x290')
+root.resizable(0,0)
+root.configure(background="black")
+
+# Title of GUI Window
+root.title("Whatsapp Automator")
+
+'''
+# Create a photoimage object of the image in the path
+image = Image.open("/Users/E.D.I.T.H/Desktop/T&T-logo/profile.png")
+test = ImageTk.PhotoImage(image)
+
+label = Label(image=test)
+label.image = test
+
+# Positioning image
+label.place(x=1, y=1)
+'''
+
+# Tkinter variable used for hour, minute, message
+hours = IntVar()
+minutes = IntVar()
+text = StringVar()
+
+# Tkinter variable used for CountryCode and phone number
+countrycode = StringVar()
+phno = StringVar()
+
+# Tkinter variable used for GroupID
+grp_id=StringVar()
+
+# Tkinter variable used for WikiSearch Query
+wiki_search = StringVar()
+
+# Tkinter variable used for YouTubeSearch Query
+yt_search = StringVar()
+
+# Tkinter variable used for GoogleSearch Query
+google_search = StringVar()
+
+# Tkinter variable used for SndrMail, SndrPass, RcvrMail and Content
+sndr_mail = StringVar()
+sndr_pass = StringVar()
+rcvr_mail = StringVar()
+cntnt = StringVar()
+
+# sendwhatmsg_to_personal()
+def send_to_personal():
+    ph = phno.get()
+    cc1 = countrycode.get()
+    cc = cc1.split('-')
+    ph = cc[0] + ph
+    root.configure(background="green")
+    pywhatkit.sendwhatmsg(ph,text.get(),hours.get(),minutes.get(),wait_time=10)
+    
+# mail()
+def mail():
+    print(sndr_mail.get())
+    print(sndr_pass.get())
+    pywhatkit.mainfunctions.sendMail(sndr_mail.get(),sndr_pass.get(),rcvr_mail.get(),cntnt.get())
+    root.configure(background="orange")
+    root.configure(title="Mail Sent")
+
+# sendwhatmsg_to_group()
+def send_to_group():
+    pywhatkit.sendwhatmsg_to_group(grp_id.get(),text.get(),hours.get(),minutes.get(),wait_time=10)
+    root.configure(background="green")
+    root.configure(title="Whatsapp Message Sent To Group")
+
+# wikiSearch()
+def wikiSearch():
+    pywhatkit.info(wiki_search.get())
+    root.configure(background="white")
+
+# Play_On_Youtube()
+def play_on_youtube():
+    pywhatkit.playonyt(yt_search.get())
+    root.configure(background="red")
+
+# googleSearch()
+def googleSearch():
+    pywhatkit.search(google_search.get())
+    root.configure(background="blue")
+
+# Label for the country_code Combo Box
+c_label = Label(root, text="Country", width = 10)
+c_label.grid(row=0, column=0) 
+c_label.configure(background="black",foreground="white")
+
+# Combo box for Country codes
+combo = Combobox(root,textvariable = countrycode, width= 18)
+combo['values'] = ("+91-India", "+92-Pakistan", "+33-France", "+90-Trukey", "+52-Mexico",
+                   '+966-Saudi Arabia', "+1-US", "+61-Australia", "+30-Greece", 
+                   '+86-China', '+49-Germany', '+852-Honk Kong', '+55-Canada', '+34-Spain', 
+                   '+7-Russia', '+380-Ukrain', '+48-Poland', '+39-Italy', 
+                   '+60-Malaysia', '+66-Thailand', '+81-Japan', '+44-UK', '+977-Nepal', 
+                   '+94-Sri Lanka', '+32-Belgium', '+880-Bangladesh') 
+combo.current(0) 
+combo.bind('<<ComboboxSelected>>') 
+combo.grid(row=0, column = 1)
+
+# Label for Phone Number
+phno_label = Label(root, text="Ph Number", width = 10) 
+phno_label.grid(row = 1 , column = 0)
+phno_label.configure(background="black",foreground="white")
+
+# Entry box for Phone Number
+phno_entry = Entry(root,textvariable = phno)
+phno_entry.grid(row= 1, column = 1)
+phno_entry.configure(background="black")
+
+# Label for Message
+text_label = Label(root,text="Message", width = 10)
+text_label.grid(row = 2, column = 0)
+text_label.configure(background="black",foreground="white")
+
+# Entry box for Message
+text_entry = Entry(root,textvariable = text)
+text_entry.grid(row=2,column = 1)
+
+# Label for Hours
+hours_label = Label(root, text="Hours", width = 10) 
+hours_label.grid(row = 3, column = 0)
+hours_label.configure(background="black",foreground="white")
+
+# Entry box for Hours
+hours_entry = Entry(root,textvariable = hours)
+hours_entry.grid(row=3,column=1)
+
+# Label for Minutes
+minutes_label = Label(root, text="Minutes", width = 10) 
+minutes_label.grid(row = 4, column = 0)
+minutes_label.configure(background="black",foreground="white")
+
+# Entry box for Minutes
+minutes_entry = Entry(root,textvariable = minutes)
+minutes_entry.grid(row=4,column=1)
+
+# Send Button which will call the sendwhatmsg_to_personal()
+generate_send_to_personal_button = Button(root, text="Send_Psnl", command=send_to_personal, width = 10) 
+generate_send_to_personal_button.grid(row=6, column=0) 
+
+# Lable for Group_ID
+id_label = Label(root, text="Group_ID", width = 10)
+id_label.grid(row=5,column=0)
+id_label.configure(background="black",foreground="white")
+
+#Entry box for Group_ID
+id_entry = Entry(root,textvariable = grp_id)
+id_entry.grid(row=5,column=1)
+
+# Send Button which will call sendwhatmsg_to_group()
+generate_send_to_group_button = Button(root, text="Send_Grp", command=send_to_group, width = 10) 
+generate_send_to_group_button.grid(row=6, column=1) 
+
+# Label for Wiki Search
+wiki_label = Label(root, text="Wiki", width = 10)
+wiki_label.grid(row=0,column=2)
+wiki_label.configure(background="black",foreground="white")
+
+# Entry box for wiki_label
+wiki_entry = Entry(root,textvariable = wiki_search)
+wiki_entry.grid(row=0,column=3)
+
+# Wiki Search Button which will call wikiSearch()
+generate_wiki_search_button = Button(root, text="Search",command=wikiSearch)
+generate_wiki_search_button.grid(row=1,column=3)
+
+# Label for YouTube Search
+yt_label = Label(root, text="YouTube", width = 10)
+yt_label.grid(row=2,column=2)
+yt_label.configure(background="black",foreground="white")
+
+# Entry box for yt_label
+yt_entry = Entry(root,textvariable = yt_search)
+yt_entry.grid(row=2,column=3)
+
+# Wiki Search Button which will call play_on_youtube()
+generate_yt_search_button = Button(root, text="Search",command=play_on_youtube)
+generate_yt_search_button.grid(row=3,column=3)
+
+# Label for Google Search
+google_label = Label(root, text="Google", width = 10)
+google_label.grid(row=4,column=2)
+google_label.configure(background="black",foreground="white")
+
+# Entry box for google_label
+google_entry = Entry(root,textvariable = google_search)
+google_entry.grid(row=4,column=3)
+
+# Wiki Search Button which will call googleSearch()
+generate_google_search_button = Button(root, text="Search",command=googleSearch)
+generate_google_search_button.grid(row=5,column=3)
+
+# Label for Sender's Mail Address
+sndr_mail_label = Label(root, text="Sender Mail", width = 10)
+sndr_mail_label.grid(row=7,column=0)
+sndr_mail_label.configure(background="black",foreground="white")
+
+# Entry box for Sender's Mail Address
+sndr_mail_entry = Entry(root,textvariable = sndr_mail)
+sndr_mail_entry.grid(row=7,column=1)
+
+# Label for Sender's Mail Password
+sndr_pass_label = Label(root, text="Sender Pass", width = 10)
+sndr_pass_label.grid(row=8,column=0)
+sndr_pass_label.configure(background="black",foreground="white")
+
+# Entry box for Sender's Mail Password
+sndr_pass_entry = Entry(root,textvariable = sndr_pass, show = '*')
+sndr_pass_entry.grid(row=8,column=1)
+
+# Label for Receiver's Mail Address
+rcvr_mail_label = Label(root, text="Receiver Mail", width = 10)
+rcvr_mail_label.grid(row=9,column=0)
+rcvr_mail_label.configure(background="black",foreground="white")
+
+# Entry box for Receiver's Mail Address
+rcvr_mail_entry = Entry(root,textvariable = rcvr_mail)
+rcvr_mail_entry.grid(row=9,column=1)
+
+# Label for Content
+cntnt_label = Label(root, text="Content", width = 10, background="black",foreground="white")
+cntnt_label.grid(row=7,column=2)
+#cntnt_label.configure(background="black",foreground="white")
+
+# Entry box for Content
+cntnt_entry = Entry(root,textvariable = cntnt)
+cntnt_entry.grid(row=7,column=3)
+
+# Send Mail Button which will call mail()
+generate_send_mail_button = Button(root, text="Send_Mail",command=mail, width = 10)
+generate_send_mail_button.grid(row=8,column=3)
+
+# Button for closing 
+exit_button = Button(root, text="Exit", command=root.destroy, width=7) 
+exit_button.grid(row=9,column=3)
+
+# START THE GUI
+root.mainloop()   
